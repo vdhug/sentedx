6c0c9c7f2a23aefa8635c236ee661eee90913cf8
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2018-12-21 15:14:57 -0200

    Specifying test database

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'postgresql': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dcobblbnu2d9rv',
         'URI': 'postgres://nauojlaaxtwsun:a817aea20ac006926ee0a25d029739de892437b9059a8fda6debcc4361f4ced4@ec2-174-129-25-182.compute-1.amazonaws.com:5432/dcobblbnu2d9rv',
@@ -85,7 +85,8 @@ DATABASES = {
         'HOST': 'ec2-174-129-25-182.compute-1.amazonaws.com',
         'PORT': '5432',
         'TEST': {
-            'NAME': 'travis_ci_test',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         },
     }
 }
